c30f225c9698f4c4d2198ebe9accba2ab4d8c094
asif <asifzaman2215s@gmail.com>
2021-10-03 22:46:44 +0600

    OPP 115 reader

--- PrivacyPolicy/OPP_115_reader.py
+++ PrivacyPolicy/OPP_115_reader.py
@@ -5,8 +5,28 @@ main_path = '/home/asif/Datasets/Privacy Policy Dataset/Usable Privacy/OPP-115_v
 annotations_path = main_path + 'annotations/'
 sanitized_policies_path = main_path + 'sanitized_policies/'
 
-if __name__ == '__main__':
+
+def info_counts():
+    info_list = []
+
     for file in os.listdir(annotations_path):
-        df = pd.read_csv(annotations_path+file,header=None)
-        print(df.to_string())
-        exit()
+        df = pd.read_csv(annotations_path + file, header=None)
+        # print(df.to_string())
+        # print(df.shape)
+        info = df.apply(lambda x: x.value_counts().shape[0])
+        info_list.append(info)
+        # exit()
+
+    df_info = pd.concat(info_list, axis=1)
+    print(df_info)
+    # print(df_info.std(axis=1))
+
+
+if __name__ == '__main__':
+    info_counts()
+    exit()
+
+    for file in os.listdir(annotations_path):
+        df = pd.read_csv(annotations_path + file, header=None)
+        # print(df.to_string())
+        # print(df.shape)
